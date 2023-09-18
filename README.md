1754b4b45f4258e0cbe977cc2e272c63acc004e5
John Gooden <johnnyg3587@gmail.com>
2023-09-17 20:47:09 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Base-neo-swerve
 Template for neo swerve drive
+Based on our 2022-23 code, includes swerve drive with NEOs and basic pathplanner auto.
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,93 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.autos.basicAuto;
+import frc.robot.commands.TeleopSwerve;
+import frc.robot.subsystems.Swerve;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  /* Controllers */
+  private final Joystick driver = new Joystick(0);
+
+  /* Drive Controls */
+  private final int translationAxis = XboxController.Axis.kLeftY.value;
+  private final int strafeAxis = XboxController.Axis.kLeftX.value;
+  private final int rotationAxis = XboxController.Axis.kRightX.value;
+  private final int speedAxis = XboxController.Axis.kRightTrigger.value;
+
+  /* Driver Buttons */
+  private final JoystickButton robotCentric =
+      new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
+  private final JoystickButton zeroGyro2 = 
+      new JoystickButton(driver, XboxController.Button.kX.value);
+  private final JoystickButton zeroGyro3 = 
+      new JoystickButton(driver, XboxController.Button.kA.value);
+  private final JoystickButton resetWheels = 
+      new JoystickButton(driver, XboxController.Button.kB.value);
+
+  private SendableChooser<Command> m_chooser = new SendableChooser<>();
+
+
+  /* Subsystems */
+  private final Swerve s_Swerve = new Swerve();
+
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    m_chooser.addOption("Basic Auto", new basicAuto(s_Swerve));
+    m_chooser.addOption("Nothing", new InstantCommand());
+    SmartDashboard.putData(m_chooser);
+    SmartDashboard.putNumber("SpeedLimit", 1);
+
+
+    s_Swerve.setDefaultCommand(
+        new TeleopSwerve(
+            s_Swerve,
+            () -> -driver.getRawAxis(translationAxis) * driver.getRawAxis(speedAxis) * SmartDashboard.getNumber("SpeedLimit", 1),
+            () -> -driver.getRawAxis(strafeAxis) * driver.getRawAxis(speedAxis) * SmartDashboard.getNumber("SpeedLimit", 1),
+            () -> -driver.getRawAxis(rotationAxis) * SmartDashboard.getNumber("SpeedLimit", 1),
+            () -> robotCentric.getAsBoolean()));
+
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    /* Driver Buttons */
+    zeroGyro2.and(zeroGyro3).onTrue(new InstantCommand(() -> s_Swerve.zeroGyro(0)));
+    resetWheels.onTrue(new InstantCommand(() -> s_Swerve.resetWheelsToAbsolute()));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // Will run in autonomous
+    return m_chooser.getSelected();
+  }
+}
